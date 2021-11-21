46cb9c99179f8227d6df83a49acc59aea25c4498
TBoubacar <boubacartoure123@gmail.com>
2021-11-21 17:00:11 +0100

    Amelioration de la création d'instance Agent

--- ProjetJava-DesignPattern-master/projetDesignPattern/src/model/AgentUsine.java
+++ ProjetJava-DesignPattern-master/projetDesignPattern/src/model/AgentUsine.java
@@ -1,7 +1,10 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Map;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
 
 import utils.ColorAgent;
 
@@ -23,23 +26,40 @@ public class AgentUsine {
 		return uniqueInstance;
 	}
 	
+	public String toString() {
+		String tmp ="";
+		for(Map.Entry<Character, AgentFactory> agent : factories.entrySet()) {
+			tmp += agent.getValue().getClass().getName().toString() + "\n";
+		}
+		return tmp;
+	}
+	
+	public ArrayList<Character> getTypes() {
+		ArrayList<Character> cles = new ArrayList<Character>();
+		for(Map.Entry<Character, AgentFactory> agent: factories.entrySet())
+			cles.add(agent.getKey());
+		return cles;
+	}
+	
 	public Agent createAgent(char type, ColorAgent color, int x, int y) {
 		// IL N'Y A PAS D'INQUIÉTUDE À CE FAIRE POUR LES DOUBLONS, CAR UN HASTABLE PERMET DE 
 		// REPÉRER LES DOUBLONS ET DONC NE L'INSERT PAS EN TANT QUE NOUVELLE DONNÉE.
-		
-		switch (type) {
-		case 'B':	// SEUL LES AGENTS BOMBERMAN SERONT EN COULEUR MALGRÉ TOUT
-			this.factories.put(type, new AgentBombermanFactory(color));
-			break;
-		case 'R':
-			this.factories.put(type, new AgentRajionFactory());
-			break;
-		case 'V':
-			this.factories.put(type, new AgentBirdFactory());
-			break;
-		default:
-			this.factories.put(type, new AgentEnnemiFactory());
-			break;
+		if(!this.getTypes().contains(type)) {
+			System.out.println("un type inexistant créé !");
+			switch (type) {
+			case 'B':	// SEUL LES AGENTS BOMBERMAN SERONT EN COULEUR MALGRÉ TOUT
+				this.factories.put(type, new AgentBombermanFactory(color));
+				break;
+			case 'R':
+				this.factories.put(type, new AgentRajionFactory());
+				break;
+			case 'V':
+				this.factories.put(type, new AgentBirdFactory());
+				break;
+			default:
+				this.factories.put(type, new AgentEnnemiFactory());
+				break;
+			}	
 		}
 		return this.factories.get(type).createAgent(x, y);
 	}
